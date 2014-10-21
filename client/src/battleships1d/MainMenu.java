c0f457383d72cb8c46fa86a55b4a609762ea5b6a
GEORGE RADUTA <k1332897@kcl.ac.uk>
2014-10-21 23:37:33 +0100

    Commit No 2 - MainMenu.java
    Arranged the code;
    Added comments;
    Added functionality to the "Set UserName" button;
    Added restriction so that if userName is empty, user will be asked to
    enter a new one;

--- client/src/battleships1d/MainMenu.java
+++ client/src/battleships1d/MainMenu.java
@@ -7,13 +7,11 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.BoxLayout;
-import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.SpringLayout;
 
 /**
  * 
@@ -23,14 +21,14 @@ import javax.swing.SpringLayout;
 public class MainMenu {
 
 	public MainMenu() {
-
 	}
 
 	JFrame mainWindow;
 	JPanel mainPanel;
 
 	/**
-	 * Creating the MainWindow
+	 * Creating the MainWindow (More like MainPanel as Fares wanted it so that
+	 * we could just switch the panels)
 	 */
 	public void createMainWindow() {
 
@@ -41,12 +39,11 @@ public class MainMenu {
 		mainWindow.setBounds(300, 100, 700, 600);
 
 		mainPanel.setLayout(new BorderLayout());
-
-		mainWindow.add(mainPanel);
-
+		
 		createNorthComponent();
 		createCenterComponent();
 
+		mainWindow.add(mainPanel);
 		// mainWindow.pack();
 		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainWindow.setVisible(true);
@@ -55,9 +52,8 @@ public class MainMenu {
 
 	JPanel northPanel;
 	JLabel titleLabel;
-
 	/**
-	 * createNorthComponent() - creates the North Component of the maineFrame;
+	 * createNorthComponent() - creates the North Component of the mainPanel;
 	 * 
 	 * @value It contains the title of the Project : "BattleShip - 1 D"
 	 */
@@ -74,69 +70,86 @@ public class MainMenu {
 
 	JPanel centerPanel;
 	JPanel firstGridPanel, secondGridPanel;
-	JPanel userNamePanel;
-	JPanel matchesPanel;
+	JPanel userNameGridPanel;
+	JPanel matchesGridPanel;
 	JLabel userNameLabel;
+	JLabel userSelectedNameLabel;
 	JButton setUserNameButton, enterLobbyButton, quickMatchButton;
+	
 	/**
+	 * createCenterComponent() - creates the Center Component of the mainPanel;
 	 * 
+	 * @value It contains the UserNameLabel, SetUserNameButton, EnterLobbyButton and QuickMathcButton;
 	 */
 	public void createCenterComponent() {
 		centerPanel = new JPanel();
 		firstGridPanel = new JPanel();
 		secondGridPanel = new JPanel();
-		userNamePanel = new JPanel();
-		matchesPanel = new JPanel();
+		userNameGridPanel = new JPanel();
+		matchesGridPanel = new JPanel();
 		
 		userNameLabel = new JLabel("UserName : ");
+		userSelectedNameLabel = new JLabel();
 		setUserNameButton = new JButton("Set UserName");
 		enterLobbyButton  = new JButton("Enter Lobby");
 		quickMatchButton  = new JButton("Quick Match");
 		
 		userNameLabel.setFont(new Font("Arial", Font.ITALIC, 16));
-		//setUserNameButton.setText("Set UserName");
+		userSelectedNameLabel.setFont(new Font("Arial", Font.BOLD, 16));
 		
-
 		centerPanel.setLayout(new GridLayout(3,1));
 		firstGridPanel.setLayout(new BorderLayout());
 		secondGridPanel.setLayout(new BorderLayout());
-		userNamePanel.setLayout(new GridLayout(2,1));
-		matchesPanel.setLayout(new GridLayout(2,1));
+		userNameGridPanel.setLayout(new GridLayout(2,1));
+		matchesGridPanel.setLayout(new GridLayout(2,1));
 		
-		mainPanel.add(centerPanel, BorderLayout.CENTER);
+		
+		
+		
+		//centerPanel.add(new JPanel());
+		
+		firstGridPanel.add(userNameGridPanel,BorderLayout.NORTH);
+		secondGridPanel.add(matchesGridPanel,BorderLayout.NORTH);
 		centerPanel.add(new JPanel());
 		centerPanel.add(firstGridPanel);
-		//centerPanel.add(new JPanel());
 		centerPanel.add(secondGridPanel);
-		firstGridPanel.add(userNamePanel,BorderLayout.NORTH);
-		secondGridPanel.add(matchesPanel,BorderLayout.NORTH);
+		mainPanel.add(centerPanel, BorderLayout.CENTER);
 		
-		JPanel moment = new JPanel();
-		moment.setLayout(new FlowLayout());
-		moment.add(userNameLabel);
-		JPanel moment2 = new JPanel();
-		moment2.setLayout(new FlowLayout());
-		moment2.add(setUserNameButton);
+		JPanel userNameLabelPanel = new JPanel();
+		userNameLabelPanel.setLayout(new FlowLayout());
+		userNameLabelPanel.add(userNameLabel);
+		userNameLabelPanel.add(userSelectedNameLabel);
 		
-		userNamePanel.add(moment);
-		userNamePanel.add(moment2);
+		JPanel setUserNameButtonPanel = new JPanel();
+		setUserNameButtonPanel.setLayout(new FlowLayout());
+		setUserNameButtonPanel.add(setUserNameButton);
 		
-		JPanel moment3 = new JPanel();
-		moment3.setLayout(new FlowLayout());
-		moment3.add(enterLobbyButton);
-		JPanel moment4 = new JPanel();
-		moment4.setLayout(new FlowLayout());
-		moment4.add(quickMatchButton);
+		userNameGridPanel.add(userNameLabelPanel);
+		userNameGridPanel.add(setUserNameButtonPanel);
 		
-		matchesPanel.add(moment3);
-		matchesPanel.add(moment4);
+		JPanel enterLobbyButtonPanel = new JPanel();
+		enterLobbyButtonPanel.setLayout(new FlowLayout());
+		enterLobbyButtonPanel.add(enterLobbyButton);
 		
+		JPanel quickMatchButtonPanel = new JPanel();
+		quickMatchButtonPanel.setLayout(new FlowLayout());
+		quickMatchButtonPanel.add(quickMatchButton);
+		
+		matchesGridPanel.add(enterLobbyButtonPanel);
+		matchesGridPanel.add(quickMatchButtonPanel);
+		
+		/**
+		 * ActionListener which will set the UserName;
+		 */
 		setUserNameButton.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				System.exit(0);
+				String userName = JOptionPane.showInputDialog(mainWindow, "Insert UserName", "Seting UserName", JOptionPane.OK_CANCEL_OPTION);
+				while (userName.toUpperCase().trim().equals("")) {
+					userName = JOptionPane.showInputDialog(mainWindow, "UserName is empty or is already taken. Please select another one.", "Seting UserName", JOptionPane.OK_CANCEL_OPTION);	
+				}
+				userSelectedNameLabel.setText(userName);
 			}
 		});
 	}
