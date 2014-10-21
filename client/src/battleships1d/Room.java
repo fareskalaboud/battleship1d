b22de588ce6f29472ceaaa32c0a64381616767d8
GEORGE RADUTA <k1332897@kcl.ac.uk>
2014-10-21 23:57:59 +0100

    Room.java Commit NO 1;
    Some parameters and methods added;
    Nothing important changed/added;

--- client/src/battleships1d/Room.java
+++ client/src/battleships1d/Room.java
@@ -1,5 +1,33 @@
 package battleships1d;
 
+/**
+ * 
+ * @author GEORGE RADUTA
+ *
+ */
 public class Room {
-
+	Player owner;
+	Map ownerMap;
+	Player guest;
+	Player guestMap;
+	
+	String roomID;
+	
+	// TO DO  :  get/set owner/guest?
+// type of room? String?  
+	// what are up.ui?
+	/**
+	 * 
+	 * @param roomID - roomID to be set;
+	 */
+	public void setRoomID(String roomID) {
+		this.roomID = roomID;
+	}
+	/**
+	 * String method
+	 * @return the ID of the Room;
+	 */
+	public String getRoomID() {
+		return this.roomID;
+	}
 }
