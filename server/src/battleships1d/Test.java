bce1a57c39f71525f077b9704b15d361ab3c32ba
Tom <thomas.owers@kcl.ac.uk>
2014-10-23 16:29:48 +0100

    Added a Testing class in which boots the server

---
+++ server/src/battleships1d/Test.java
@@ -0,0 +1,11 @@
+package battleships1d;
+
+/**
+ * Created by Thomas on 21/10/2014.
+ */
+public class Test {
+    public static void main(String[] args) {
+        UserManager.init();
+        ServerManager.start();
+    }
+}
