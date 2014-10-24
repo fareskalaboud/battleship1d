30fffe59c7eb541af77f105fed38e220fd78fabe
Fares Alaboud <fares.alaboud@gmail.com>
2014-10-24 14:48:32 +0100

    Created two instances of Map: one for the local player and one for the enemy player

---
+++ client/src/battleships1d/EnemyMap.java
@@ -0,0 +1,57 @@
+package battleships1d;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ *
+ * @author faresalaboud
+ */
+public class EnemyMap extends Map {
+
+    public EnemyMap() {
+        super();
+        // TODO: Fix next line
+        this.setUpUI(new GameButton(1,1));
+
+    }
+
+    public static void main(String args[]) {
+        EnemyMap map = new EnemyMap();
+        JFrame frame = new JFrame();
+        frame.add(map);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+    }
+
+    public void setUpHealthBar() {
+
+    }
+
+    public void setUpUI(GameButton btn) {
+        this.setLayout(new BorderLayout());
+        this.setSize(300, 300);
+
+        // Initialise UI objects
+        mapPanel = new JPanel(new GridLayout(10, 10));
+        healthPanel = new JPanel();
+        healthBar = new JProgressBar(0, 17);
+
+        // Set up health bar
+        healthBar.setForeground(new Color(169, 107, 39));
+        healthBar.setValue(healthBar.getMaximum());
+//        healthBar.setMaximumSize();
+
+        // Start adding everything to the panels
+        healthPanel.add(healthBar);
+
+        // Add the panels to the map
+        this.add(mapPanel, BorderLayout.CENTER);
+        mapPanel.add(healthPanel, BorderLayout.NORTH);
+
+    }
+
+    public void setActionListeners() {
+
+    }
+}
--- client/src/battleships1d/Map.java
+++ client/src/battleships1d/Map.java
@@ -1,9 +1,24 @@
 package battleships1d;
 
-public class Map {
-    private GameButton[][] mapTiles;
-    private Ship[] ships;
+import javax.swing.*;
 
+/**
+ * @author faresalaboud
+ */
+public abstract class Map extends JPanel {
+    protected UIManager uiManager;
 
+    protected GameButton[][] mapTiles;
+    //private Ship[] ships;
+
+    protected JPanel mapPanel;
+    protected JPanel healthPanel;
+    protected JProgressBar healthBar;
+
+    public abstract void setUpHealthBar();
+
+    public abstract void setUpUI(GameButton btn);
+
+    public abstract void setActionListeners();
 
 }
