72d91be063477a3106a43487bba7add0248b14ef
Alexander Hanbury-Botherway <alexander.hanbury-botherway@kcl.ac.uk>
2014-11-06 15:51:45 +0000

    RoomData class added which retrieves roomID from server when adding itself

--- client/src/battleships1d/AppManager.java
+++ client/src/battleships1d/AppManager.java
@@ -9,194 +9,196 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 /**
- *
+ * 
  * @author faresalaboud
- *
+ * 
  */
 public class AppManager {
-    // UI Variables
-    private static GameFrame frame;
+	// UI Variables
+	private static GameFrame frame;
 
-    // Player Variables
-    private static String mainPlayer;
+	// Player Variables
+	private static String mainPlayer;
 
-    // Lobby Variables
-    private static Vector<Room> publicRooms;
-    private static Vector<Room> privateRooms;
+	// Lobby Variables
+	private static Vector<Room> publicRooms;
+	private static Vector<Room> privateRooms;
 
-    // Game Variables
-    private boolean isLocalMove;
-    private Room openRoom;
+	// Game Variables
+	private boolean isLocalMove;
+	private Room openRoom;
 
-    // UI Management
+	// UI Management
 
-    /**
-     * Set up the main GameFrame (subclass of JFrame)
-     *
-     * @see battleships1d.GameFrame
-     * @author faresalaboud
-     */
-    public static void setUpUI() {
-        frame = new GameFrame("Battleships 1D");
-    }
+	/**
+	 * Set up the main GameFrame (subclass of JFrame)
+	 * 
+	 * @see battleships1d.GameFrame
+	 * @author faresalaboud
+	 */
+	public static void setUpUI() {
+		frame = new GameFrame("Battleships 1D");
+	}
 
-    // Player Management
+	// Player Management
 
-    /**
-     * Returns the local player.
-     *
-     * @return the mainPlayer object
-     * @author faresalaboud
-     */
-    public static String getMainPlayer() {
-        return mainPlayer;
-    }
+	/**
+	 * Returns the local player.
+	 * 
+	 * @return the mainPlayer object
+	 * @author faresalaboud
+	 */
+	public static String getMainPlayer() {
+		return mainPlayer;
+	}
 
-    /**
-     * Checks the server if the player's login details are correct or not.
-     *
-     * @param username Player's username
-     * @param password Player's password
-     *
-     * @return the server response
-     */
-    public static String checkPlayerLoginDetails(String username, String password) {
+	/**
+	 * Checks the server if the player's login details are correct or not.
+	 * 
+	 * @param username
+	 *            Player's username
+	 * @param password
+	 *            Player's password
+	 * 
+	 * @return the server response
+	 */
+	public static String checkPlayerLoginDetails(String username,
+			String password) {
 
-        ArrayList<String> commands = new ArrayList<String>();
-        commands.add("Login::User::Successful::");
-        commands.add("Login::User::Error::Username");
-        commands.add("Login::User::Error::Password");
+		ArrayList<String> commands = new ArrayList<String>();
+		commands.add("Login::User::Successful::");
+		commands.add("Login::User::Error::Username");
+		commands.add("Login::User::Error::Password");
 
-        final Server.RequestVariables rv = new Server.RequestVariables();
+		final Server.RequestVariables rv = new Server.RequestVariables();
 
-        Server.registerCommands(commands, new Server.RequestFunction() {
-            @Override
-            public void Response(String command) {
-                rv.setCommand(command);
-                rv.setContinueThread(true);
-            }
-        });
+		Server.registerCommands(commands, new Server.RequestFunction() {
+			@Override
+			public void Response(String command) {
+				rv.setCommand(command);
+				rv.setContinueThread(true);
+			}
+		});
 
-        Server.writeLineToServer("Login::User::" + username + "::" + password);
-        waitOnThread(rv);
+		Server.writeLineToServer("Login::User::" + username + "::" + password);
+		waitOnThread(rv);
 
-        return rv.getCommand();
+		return rv.getCommand();
 
-        //TODO: Send to server, and fix line below
-//        String response = "hello";
-//
-//        String[] serverResponse = response.split("::");
-//        if(serverResponse[2].equals("Successful")) {
-//            return "Successful";
-//        } else if (serverResponse[2].equals("Error")){
-//            if (serverResponse.length == 3) {
-//                return "Error";
-//            } else {
-//                return "Error::" + username;
-//            }
-//        }
-//        return "Error";
-    }
+		// TODO: Send to server, and fix line below
+		// String response = "hello";
+		//
+		// String[] serverResponse = response.split("::");
+		// if(serverResponse[2].equals("Successful")) {
+		// return "Successful";
+		// } else if (serverResponse[2].equals("Error")){
+		// if (serverResponse.length == 3) {
+		// return "Error";
+		// } else {
+		// return "Error::" + username;
+		// }
+		// }
+		// return "Error";
+	}
 
-    public static String createAccount(String username, String password) {
-    	Server.writeLineToServer("Login::Create::" + username + "::" + password);
-        //TODO: Send to server, and fix line below
-        String response = "hello";
+	public static String createAccount(String username, String password) {
+		Server.writeLineToServer("Login::Create::" + username + "::" + password);
+		// TODO: Send to server, and fix line below
+		String response = "hello";
 
-        String[] serverResponse = response.split("::");
-        if(serverResponse[2].equals("Successful")) {
-            return "Successful";
-        } else if (serverResponse[2].equals("Error")){
-            if (serverResponse.length == 3) {
-                return "Error";
-            } else {
-                return "Error::" + username;
-            }
-        }
-        return "Error";
-    }
+		String[] serverResponse = response.split("::");
+		if (serverResponse[2].equals("Successful")) {
+			return "Successful";
+		} else if (serverResponse[2].equals("Error")) {
+			if (serverResponse.length == 3) {
+				return "Error";
+			} else {
+				return "Error::" + username;
+			}
+		}
+		return "Error";
+	}
 
-    /**
-     * Sets the main player according to mainPlayer's current value
-     * (if it's null, assign it a Guest value; else assign it the logged
-     * in user.)
-     *
-     * @author faresalaboud
-     */
-    public static void setMainPlayer() {
-        if (mainPlayer == null) {
-            // If mainPlayer is null, then the game has only just booted up.
-            // TODO: Obtain guest player object from server and assign it to mainPlayer
-        } else {
-            // If mainPlayer isn't null, it is most likely a Guest user.
-            // TODO: Assign logged-in player to mainPlayer
-        }
-    }
+	/**
+	 * Sets the main player according to mainPlayer's current value (if it's
+	 * null, assign it a Guest value; else assign it the logged in user.)
+	 * 
+	 * @author faresalaboud
+	 */
+	public static void setMainPlayer() {
+		if (mainPlayer == null) {
+			// If mainPlayer is null, then the game has only just booted up.
+			// TODO: Obtain guest player object from server and assign it to
+			// mainPlayer
+		} else {
+			// If mainPlayer isn't null, it is most likely a Guest user.
+			// TODO: Assign logged-in player to mainPlayer
+		}
+	}
 
-    // Server Management
+	// Server Management
 
-    /**
-     * Obtains a list of the rooms from the server and adds them to
-     * the public and private room Vectors.
-     *
-     * @author faresalaboud
-     */
+	/**
+	 * Obtains a list of the rooms from the server and adds them to the public
+	 * and private room Vectors.
+	 * 
+	 * @author faresalaboud
+	 */
 
-    public static void getRoomsFromServer() {
-        // TODO: Remove initialisation, obtain rooms from server
-        Vector<Room> allRooms = new Vector<Room>();
+	public static void getRoomsFromServer() {
+		// TODO: Remove initialisation, obtain rooms from server
+		Vector<Room> allRooms = new Vector<Room>();
 
-        // TODO: Command.getParameters
-        String[] parameters = new String[0];
-        Room r;
+		// TODO: Command.getParameters
+		String[] parameters = new String[0];
+		Room r;
 
-        int noOfRooms = Integer.parseInt(parameters[0]);
-        String roomID = new String();
-        String userName = new String();
-        String password = new String();
+		int noOfRooms = Integer.parseInt(parameters[0]);
+		String roomID = new String();
+		String userName = new String();
+		String password = new String();
 
-        for (int i = 1; i < parameters.length; i++) {
-            if (i % 3 == 1) {
-                roomID = parameters[i];
-            } else if (i % 3 == 2) {
-                userName = parameters[i];
-            } else {
-                password = parameters[i];
-                if (password.equals("")) {
-                    r = new Room(roomID, userName);
-                    allRooms.add(r);
-                    publicRooms.add(r);
-                } else {
-                    r = new Room(roomID, password, userName);
-                    allRooms.add(r);
-                    privateRooms.add(r);
-                }
-            }
-        }
-    }
+		for (int i = 1; i < parameters.length; i++) {
+			if (i % 3 == 1) {
+				roomID = parameters[i];
+			} else if (i % 3 == 2) {
+				userName = parameters[i];
+			} else {
+				password = parameters[i];
+				if (password.equals("")) {
+					r = new Room(roomID, userName);
+					allRooms.add(r);
+					publicRooms.add(r);
+				} else {
+					r = new Room(roomID, password, userName);
+					allRooms.add(r);
+					privateRooms.add(r);
+				}
+			}
+		}
+	}
 
-    /**
-     * Returns a Vector of public rooms.
-     *
-     * @return a Vector of public rooms
-     * @author faresalaboud
-     */
-    public static Vector<Room> getPublicRooms() {
-        return publicRooms;
-    }
+	/**
+	 * Returns a Vector of public rooms.
+	 * 
+	 * @return a Vector of public rooms
+	 * @author faresalaboud
+	 */
+	public static Vector<Room> getPublicRooms() {
+		return publicRooms;
+	}
 
-    /**
-     * Returns a Vector of private rooms.
-     *
-     * @return a Vector of private rooms
-     * @author faresalaboud
-     */
-    public static Vector<Room> getPrivateRooms() {
-        return privateRooms;
-    }
+	/**
+	 * Returns a Vector of private rooms.
+	 * 
+	 * @return a Vector of private rooms
+	 * @author faresalaboud
+	 */
+	public static Vector<Room> getPrivateRooms() {
+		return privateRooms;
+	}
 
-    
-    /**
+	/**
 	 * 
 	 * @param row
 	 * @param col
@@ -214,70 +216,97 @@ public class AppManager {
 			System.err.println("IOException");
 			return null;
 		}
-		if (returnedResult.equals("Game::Fire")){
+		if (returnedResult.equals("Game::Fire")) {
 			return Result.MISS;
-		} if (returnedResult.equals("Game::Hit")){
+		}
+		if (returnedResult.equals("Game::Hit")) {
 			return Result.HIT;
-		} if (returnedResult.equals("Game::Sunk")){
+		}
+		if (returnedResult.equals("Game::Sunk")) {
 			return Result.SUNK;
 		}
-		System.err.println("Result from button: " + row + " " + col + " is not valid");
-		return null; 
+		System.err.println("Result from button: " + row + " " + col
+				+ " is not valid");
+		return null;
 	}
-	
+
 	/**
 	 * Called if server sends a players move
+	 * 
 	 * @param rv
 	 * @return
 	 */
-	public void sendMove(int row, int col){
+	public void sendMove(int row, int col) {
 		LocalButton targetButton = openRoom.getLocalMap().getButton(row, col);
-		
+
 		Result result = targetButton.playButton();
-		
-		if (result.equals(Result.HIT)){
-			new JDialog (new JFrame(), "You've been hit!");
+
+		if (result.equals(Result.HIT)) {
+			new JDialog(new JFrame(), "You've been hit!");
 			return;
-		} if (result.equals(Result.SUNK)){
+		}
+		if (result.equals(Result.SUNK)) {
 			String shipName = targetButton.getShip().getName();
-			new JDialog (new JFrame(), "The enemy has sunk your " + shipName + "!");
+			new JDialog(new JFrame(), "The enemy has sunk your " + shipName
+					+ "!");
 			return;
-		} if (result.equals(Result.MISS)){
-			new JDialog (new JFrame(), "Enemy failed to hit a ship: Your move!");
+		}
+		if (result.equals(Result.MISS)) {
+			new JDialog(new JFrame(), "Enemy failed to hit a ship: Your move!");
 			return;
 		} else {
-		System.err.println("Unexpected response from local button after being hit");
-		return;
+			System.err
+					.println("Unexpected response from local button after being hit");
+			return;
 		}
 	}
 
-    private static Boolean waitOnThread(Server.RequestVariables rv) {
-        while(!rv.getContinueThread()) {
-            try {
-                Thread.sleep(50);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        return true;
-    }
+	private static Boolean waitOnThread(Server.RequestVariables rv) {
+		while (!rv.getContinueThread()) {
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		return true;
+	}
 
-    public static void main(String args[]) {
-    	if (Server.connectToServer()) {
-            setUpUI();
-            setMainPlayer();
-           // getRoomsFromServer();
-           // frame.setVisible(true);
-            new LogIn().setUpUI();
+	public static void main(String args[]) {
+		if (Server.connectToServer()) {
+			setUpUI();
+			setMainPlayer();
+			// getRoomsFromServer();
+			// frame.setVisible(true);
+			new LogIn().setUpUI();
 
-        } else {
-        	
-            JOptionPane.showMessageDialog(new JFrame(),
-                    "Please ensure you have a working internet connection.",
-                    "Error: Unable to connect to server",
-                    JOptionPane.ERROR_MESSAGE);
-            System.exit(1);
-        }
+		} else {
 
-    }
+			JOptionPane.showMessageDialog(new JFrame(),
+					"Please ensure you have a working internet connection.",
+					"Error: Unable to connect to server",
+					JOptionPane.ERROR_MESSAGE);
+			System.exit(1);
+		}
+
+	}
+
+	/**
+	 * @author Alexander Hanbury-Botherway
+	 * @param password
+	 * @return
+	 */
+	public static String createRoom(String password) {
+		Server.writeLineToServer("Room::Create:: + password");
+
+		String response = "Test";
+
+		String[] serverResponse = response.split("::");
+		if (serverResponse[2].equals("Errot")) {
+			return "Error";
+		} else {
+			return serverResponse[2];
+		}
+
+	}
 }
---
+++ client/src/battleships1d/RoomData.java
@@ -0,0 +1,33 @@
+package battleships1d;
+
+public class RoomData {
+	private String roomID;
+	private String roomName;
+	private boolean isPrivate;
+	private String password;
+	
+	public RoomData(String roomName, boolean isPrivate, String password){
+		this.roomName = roomName;
+		this.isPrivate = isPrivate;
+		if (isPrivate){
+		this.password = password;
+		} else {
+			password = "";
+		}
+		String serverResponse = AppManager.createRoom(password);
+		
+		if (serverResponse.equals("Error")){
+			System.err.println("Error when creating room: room already in use?");
+			roomName += " (FAILED TO SYNC WITH SERVER)";
+		} else {
+			roomID = serverResponse;
+		}
+	}
+	
+	public String toString(){
+		return roomName + " (" + roomID + ")" ;
+	}
+	
+	
+	
+}
