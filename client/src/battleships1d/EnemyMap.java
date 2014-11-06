f8f329d427a0ec217587b2a5bcebe4e60e200be2
NotBeatrice <k1333495@kcl.ac.uk>
2014-11-06 17:06:16 +0000

    Fixed a few things in enemy map. Added some 2d arrays to prevent double clicking, added a ready button

--- client/src/battleships1d/EnemyMap.java
+++ client/src/battleships1d/EnemyMap.java
@@ -6,6 +6,7 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
@@ -17,12 +18,19 @@ import javax.swing.JProgressBar;
 public class EnemyMap extends Map {
 
 	EnemyButton[][] enemyButtons;
+	boolean[][] hasBeenClicked;
 	
 	
     public EnemyMap(Room room) {
         super();
         this.room = room;
         enemyButtons = new EnemyButton[10][10];
+        hasBeenClicked = new boolean[10][10];
+        for(int i = 0; i < 10; i++){
+        	for(int j = 0; j < 10; j++){
+        		hasBeenClicked[i][j] = false;
+        	}
+        }
         
         
         this.setUpUI();
@@ -36,9 +44,34 @@ public class EnemyMap extends Map {
     public void setUpUI() {
         this.setLayout(new BorderLayout());
         this.setSize(300, 300);
+        
+        
 
         // Initialise UI objects
         mapPanel = new JPanel(new GridLayout(10, 10));
+        
+        for(int i = 0; i < 10; i++){
+        	for(int j = 0; j < 10; j++){
+        		enemyButtons[i][j] = new EnemyButton(i, j, room);
+        		enemyButtons[i][j].setBackground(Color.black);
+        		mapPanel.add(enemyButtons[i][j]);
+        		
+        	}
+        }
+        
+        
+        JButton ready = new JButton("Ready");
+        ready.addActionListener(new ActionListener(){
+        	
+        	@Override
+        	public void actionPerformed(ActionEvent arg0) {
+        		// TODO Auto-generated method stub
+        		addButtons();
+        	}
+        	
+        });
+        
+        
         healthPanel = new JPanel();
         healthBar = new JProgressBar(0, 17);
 
@@ -53,30 +86,36 @@ public class EnemyMap extends Map {
         // Add the panels to the map
         this.add(mapPanel, BorderLayout.CENTER);
         add(healthPanel, BorderLayout.NORTH);
+        add(ready, BorderLayout.SOUTH);
         
-        addButtons();
+        
 
     }
     
+    
+    //only add buttons when room is established, "ready" 
+    //could also be in the Room class
     public void addButtons(){
     	for (int i = 0; i<10; i++){
     		for (int j = 0; j<10; j++){
     			final int row = i;
     			final int column = j;
-    			enemyButtons[i][j] = new EnemyButton(i, j, room);
     			enemyButtons[i][j].setBackground(Color.gray);
     			enemyButtons[i][j].addActionListener(new ActionListener(){
 
 					@Override
 					public void actionPerformed(ActionEvent arg0) {
 						// TODO Auto-generated method stub
-						enemyButtons[row][column].playButton();
-						updateHealthBars();
+						if(!hasBeenClicked[row][column]){
+							enemyButtons[row][column].playButton();
+							hasBeenClicked[row][column] = true;
+							updateHealthBars();
+						}
 					}
     				
     			});
     			
-    			mapPanel.add(enemyButtons[i][j]);
+    			
     		}
     	}
     }
